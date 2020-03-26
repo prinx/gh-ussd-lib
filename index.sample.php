680853a2821f672bd557ef974d3b1332c50dc4ee
CinPer <damien.dorcis@yahoo.com>
2020-03-26 14:15:37 +0000

    Update index.sample.php

--- index.sample.php
+++ index.sample.php
@@ -1,20 +1,46 @@
 <?php
+/**
+ * (c) Nuna Akpaglo <princedorcis@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+require_once __DIR__ . '/vendor/prinx/ussd-lib/src/USSD.php';
+
+use function Prinx\Dotenv\env;
+use Prinx\USSD\USSD;
+
 define('ENV', 'development');
 
-require_once "lib/ussd.lib.php";
-
-class App
+class USSDApp
 {
-    protected $params = [];
+    protected $default_date_format = 'd/m/Y';
+
+    protected $app_params = [
+        'id' => 'first_ussd_app',
+        'environment' => 'dev',
+        'end_on_user_error' => false,
+
+        'always_start_new_session' => false,
+        'ask_user_before_reload_last_session' => true,
+
+        'always_send_sms' => true,
+        'sms_sender_name' => 'BASICAPP',
+        // 'sms_endpoint' => '',
+
+        'back_action_thrower' => '0',
+        'back_action_display' => 'Back',
+        'splitted_menu_next_thrower' => '99',
+        'splitted_menu_display' => 'More',
+
+        'default_end_msg' => 'Thank you.',
+        'default_error_msg' => 'Invalid Input.',
+    ];
+
     protected $ussd;
-    
-    /*
-    Put messages into double quotes rather than single quotes if you wanna
-    use "\n" inside the message.
-    Using single quotes will display the character '\n' in the message instead of displying a newline.
-     */
+
     protected $menus = [
-        // the menu named 'welcome' is required
         'welcome' => [
             'message' => "Welcome.\nSelect an option",
             'actions' => [
@@ -24,9 +50,13 @@ class App
                 ],
                 '2' => [
                     'display' => 'What is the date?',
-                    'next_menu' => 'get_time',
+                    'next_menu' => 'show_date',
                 ],
                 '3' => [
+                    'display' => 'Caluculate age',
+                    'next_menu' => 'get_birthdate',
+                ],
+                '4' => [
                     'display' => 'Say Goodbye',
                     'next_menu' => 'say_goodbye',
                 ],
@@ -35,20 +65,48 @@ class App
 
         'verify_working' => [
             'message' => "Of course, I'm working!",
+        ],
+
+        'show_date' => [
+            'message' => "Today is :date:!",
+            'actions' => [
+                '1' => [
+                    'display' => 'Back',
+                    'next_menu' => '__back',
+                ],
+                '0' => [
+                    'display' => 'End',
+                    'next_menu' => '__end',
+                ],
+            ],
+        ],
+
+        'get_birthdate' => [
+            'message' => "Enter your birthdate (dd/mm/yyyy)\nOr enter 0 to go back:",
             'actions' => [
                 '0' => [
                     'display' => 'Back',
                     'next_menu' => '__back',
                 ],
+
+                'default_next_menu' => 'show_age',
             ],
         ],
 
-        'get_time' => [
-            'message' => "Today is :date:!",
+        'show_age' => [
+            'message' => "You are :age: years old!",
             'actions' => [
                 '0' => [
                     'display' => 'Back',
                     'next_menu' => '__back',
+                ],
+                '1' => [
+                    'display' => 'Main menu',
+                    'next_menu' => '__welcome',
+                ],
+                '2' => [
+                    'display' => 'End',
+                    'next_menu' => '__end',
                 ],
             ],
         ],
@@ -60,45 +118,97 @@ class App
 
     public function __construct()
     {
-        $this->params['id'] = 'basic_app';
-        $this->params['environment'] = 'dev';
-
-        $this->ussd = new USSDLib();
+        $this->ussd = new USSD();
         $this->ussd->run($this);
     }
 
-    public function before_get_time($user_responses)
+    public function before_show_date()
     {
-        return [
-            'date' => date('d-m-Y'),
-        ];
+        return ['date' => date('D-m-Y')];
+    }
+
+    public function create_date_from_format($date, $format = '')
+    {
+        $format = $format !== '' ? $format : $this->default_date_format;
+
+        return DateTime::createFromFormat($format, $date);
+    }
+
+    public function is_valid_date($date)
+    {
+        $year = $date->format('Y');
+        $month = $date->format('m');
+        $day = $date->format('d');
+
+        return checkdate($month, $day, $year);
+    }
+
+    public function validate_get_birthdate($response)
+    {
+        $date = $this->create_date_from_format($response);
+
+        if ($date === false) {
+            $this->ussd->set_error('Invalid birthdate format.');
+            return false;
+        }
+
+        $min = 0;
+        $max = 150;
+        $age = $this->calculate_age($response);
+
+        if (!$this->is_valid_date($date) || !$this->age_within($min, $max, $age)) {
+            $this->ussd->set_error('Invalid birthdate.');
+            return false;
+        }
+
+        return true;
+    }
+
+    public function age_within(int $min, int $max, int $age)
+    {
+        return $min <= $age && $age < $max;
+    }
+
+    public function calculate_age($birthdate, $birthdate_format = 'd/m/Y')
+    {
+        return DateTime::createFromFormat($birthdate_format, $birthdate)
+            ->diff(new DateTime('now'))
+            ->y;
+    }
+
+    public function before_show_age($user_previous_response)
+    {
+        $birthdate = $user_previous_response['get_birthdate'][0];
+
+        $age = $this->calculate_age($birthdate);
+
+        return ['age' => $age];
     }
 
     public function db_params()
     {
-        $config = [
-            'driver' => 'mysql',
-            'username' => 'root',
-            'password' => '',
-        ];
+        $config = [];
 
         if (ENV !== 'production') {
-            $config['hostname'] = 'localhost';
-            $config['password'] = '';
-            $config['dbname'] = '';
+            $config['username'] = env('DEV_DB_USER');
+            $config['password'] = env('DEV_DB_PASS');
+            $config['hostname'] = env('DEV_DB_HOST');
+            $config['port'] = env('DEV_DB_PORT');
+            $config['dbname'] = env('DEV_DB_NAME');
         } else {
-            $config['hostname'] = '';
-            $config['password'] = '';
-            $config['port'] = '';
-            $config['dbname'] = '';
+            $config['username'] = env('PROD_DB_USER');
+            $config['password'] = env('PROD_DB_PASS');
+            $config['hostname'] = env('PROD_DB_HOST');
+            $config['port'] = env('PROD_DB_PORT');
+            $config['dbname'] = env('PROD_DB_NAME');
         }
 
         return $config;
     }
 
-    public function params()
+    public function app_params()
     {
-        return $this->params;
+        return $this->app_params;
     }
 
     public function menus()
@@ -107,4 +217,4 @@ class App
     }
 }
 
-$app = new App();
+$app = new USSDApp();
